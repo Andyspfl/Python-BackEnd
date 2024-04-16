43dfeb46bbd9001d565371d802078c53cb3510e8
Andyspfl <andymaytac@gmail.com>
2024-04-16 02:07:37 -0500

    update-django-crud

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## Notas sobre cosas que a mi parecer es algo complicado de usar
 
-aun me quede en 46:53
---
+++ django_crud-Project/tasks/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse 
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, "signUp.html", {"form":UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #resgister user
+           try:
+                user =User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+           except IntegrityError:
+               return render(request, "signUp.html", {"form":UserCreationForm,"Error":"Username already exists"})
+        return render(request, "signUp.html", {"form":UserCreationForm,"Error":"Password do not match"})
+        
+def tasks(request):
+    tasks =Task.objects.all()
+    return render(request, "tasks.html",{'task': tasks})
+
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{
+        'form':AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html',{
+                'form':AuthenticationForm,
+                'Error': "username or password is incorrect"
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, "create_task.html",{'form':TaskForm})
+    else:
+        try:
+            form=TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            print(new_task)
+            return redirect('tasks')
+        except ValueError:
+            return render(request, "create_task.html",{'form':TaskForm,
+            'Error': "please introducing valide data"})
