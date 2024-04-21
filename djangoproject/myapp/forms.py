9156c6b1004bdb83f5f2213e8e499316f2adcd37
Andyspfl <andymaytac@gmail.com>
2024-04-21 00:15:46 -0500

    reloading

--- djangoproject/myapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class CreateNewTask(forms.Form):
-
-    title = forms.CharField(label='Título de la tarea', max_length=200)
-    description = forms.CharField(label='Descripción de la tarea', widget=forms.Textarea, required=False)
-class CreateNewProject(forms.Form):
-    name = forms.CharField(label="nombre del project", max_length=100)
